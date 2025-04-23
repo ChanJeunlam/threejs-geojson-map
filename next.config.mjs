3d6516cd1a2b199f73f5e20cb84c2bfa68be9f11
2907 <82876040+ChanJeunlam@users.noreply.github.com>
2025-04-23 09:31:37 +0000

    修改了部署文件

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // 使用静态导出
-  basePath: "/threejs-geojson-map", // 设置仓库名称为 basePath
+  basePath: process.env.NODE_ENV === "production" ? "/threejs-geojson-map" : "", // 仅在生产环境启用 basePath
 }
 
 export default nextConfig
