3f918fb6a52419d31a41023606e239ac7f7e9c72
2907 <82876040+ChanJeunlam@users.noreply.github.com>
2025-04-23 09:23:22 +0000

    next.config.mjs添加basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // 使用静态导出
+  basePath: "/threejs-geojson-map", // 设置仓库名称为 basePath
 }
 
 export default nextConfig
