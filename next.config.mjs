730bb387978256681934107399c565cfe4155015
2907 <82876040+ChanJeunlam@users.noreply.github.com>
2025-04-23 09:55:37 +0000

    修改部署文件

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/threejs-geojson-map',
+  output: "export", // 使用静态导出
+  basePath: process.env.NODE_ENV === "production" ? "/threejs-geojson-map" : "", // 仅在生产环境启用 basePath
   assetPrefix: '/threejs-geojson-map/',  // 添加此行
-  images: {
-    unoptimized: true,
-  },
   trailingSlash: true,  // 添加此行以确保URL末尾有斜杠
-};
 
-export default nextConfig;
+}
+
+export default nextConfig
