e59c9376beedf1e33dd4db995b781d0509d8d915
2907 <82876040+ChanJeunlam@users.noreply.github.com>
2025-04-23 09:47:33 +0000

    确定资源引用正确

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // 使用静态导出
-  basePath: process.env.NODE_ENV === "production" ? "/threejs-geojson-map" : "", // 仅在生产环境启用 basePath
-}
+  output: 'export',
+  basePath: '/threejs-geojson-map',
+  assetPrefix: '/threejs-geojson-map/',  // 添加此行
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,  // 添加此行以确保URL末尾有斜杠
+};
 
-export default nextConfig
+export default nextConfig;
