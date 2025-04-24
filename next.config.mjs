4c837c7d3094fb9330682b9a50c62ef664bd5135
2907 <82876040+ChanJeunlam@users.noreply.github.com>
2025-04-24 00:36:48 +0000

    解决资源路径问题

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,8 @@
 const nextConfig = {
   output: "export", // 使用静态导出
   basePath: process.env.NODE_ENV === "production" ? "/threejs-geojson-map" : "", // 仅在生产环境启用 basePath
-  assetPrefix: '/threejs-geojson-map/',  // 添加此行
-  trailingSlash: true,  // 添加此行以确保URL末尾有斜杠
-
+  assetPrefix: process.env.NODE_ENV === "production" ? "/threejs-geojson-map/" : "", // 仅在生产环境启用 assetPrefix
+  trailingSlash: true, // 确保生成的路径以斜杠结尾
 }
 
 export default nextConfig
